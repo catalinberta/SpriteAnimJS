4e876e29d95180b7dd1262c7245ca748921d4a97
Catalin Berta <catalinberta@yahoo.com>
2015-05-22 17:15:57 +0300

    preloadjs + start/stop methods

--- dist/spriteanim-1.0.js
+++ dist/spriteanim-1.0.js
@@ -1,39 +1,50 @@
-function SpriteAnim(canvasId,spriteObj) {
+function SpriteAnim(canvasId) {
 	// Canvas
 	this.canvas = document.getElementById(canvasId); // Select given canvas ID
 	this.context = this.canvas.getContext("2d"); // Get context
-	this.canvas.width = spriteObj.frameWidth; // Set canvas width
-	this.canvas.height = spriteObj.frameHeight; // Set canvas width
 
-	// Sprite Info
-	this.width = spriteObj.image.width; // Sprite total width
-	this.height = spriteObj.image.height; // Sprite total height
-	this.image = spriteObj.image; // Sprite image
+	this.start = function(spriteObj) {
+		this.spriteObj = spriteObj;
+		this.canvas.width = this.spriteObj.frameWidth; // Set canvas width
+		this.canvas.height = this.spriteObj.frameHeight; // Set canvas width
 
-	// FPS stuff
-	this.fps = spriteObj.fps;
-	this.timestamp_init = Date.now(); // Before ticker starts
-	this.interval = 1000 / this.fps; // Frame's interval in ms 
-	this.timestamp_now, this.delta; // Vars
-	
-	// Frame stuff
-	this.frameIndex = 0; // Frame index
-	this.numberOfFrames = (this.width / spriteObj.frameWidth) || 1; // Number of the spritesheet's total frames (horizontally for now, stay tuned)
-	this.loopSprite = spriteObj.loop || false; // If should loop boolean
-	this.playSprite = true; // Play state boolean
+		// Sprite Info
+		this.width = this.spriteObj.image.width; // Sprite total width
+		this.height = this.spriteObj.image.height; // Sprite total height
+		this.image = this.spriteObj.image; // Sprite image
+
+		// FPS stuff
+		this.fps = this.spriteObj.fps;
+		this.timestamp_init = Date.now(); // Before ticker starts
+		this.interval = 1000 / this.fps; // Frame's interval in ms 
+		this.timestamp_now, this.delta; // Vars
+		
+		// Frame stuff
+		this.frameIndex = 0; // Frame index
+		this.numberOfFrames = (this.width / spriteObj.frameWidth) || 1; // Number of the spritesheet's total frames (horizontally for now, stay tuned)
+		this.loopSprite = this.spriteObj.loop || false; // If should loop boolean
+		this.playSprite = true; // Play state boolean
+
+		this.onStart(); //onStart callback function
+	}
+
+	this.stop = function() {
+		this.playSprite = false;
+		this.context.clearRect(0, 0, this.width, this.height);
+	}
 
 	// On start callback
 	this.onStart = function() {
 		this.ticker();
-		if(spriteObj.onStart) {
-			spriteObj.onStart();
+		if(this.spriteObj.onStart) {
+			this.spriteObj.onStart();
 		}
 	};
 
 	// On complete callback
 	this.onComplete = function() {
-		if(spriteObj.onComplete) {
-			spriteObj.onComplete();
+		if(this.spriteObj.onComplete) {
+			this.spriteObj.onComplete();
 		}
 	};
 
@@ -69,14 +80,14 @@ function SpriteAnim(canvasId,spriteObj) {
 	this.ticker = function() {
 		if(this.playSprite) {
 			window.requestAnimationFrame(this.ticker.bind(this));
-		}
-		this.timestamp_now = Date.now();
-	    this.delta = this.timestamp_now - this.timestamp_init;
-	    
-	    if (this.delta > this.interval) {
-	    	this.timestamp_init = this.timestamp_now - (this.delta % this.interval);
-			this.update();
+			this.timestamp_now = Date.now();
+		    this.delta = this.timestamp_now - this.timestamp_init;
+		    
+		    if (this.delta > this.interval) {
+		    	this.timestamp_init = this.timestamp_now - (this.delta % this.interval);
+				this.update();
+			}
 		}
 	};
-	this.onStart();
+	
 }
